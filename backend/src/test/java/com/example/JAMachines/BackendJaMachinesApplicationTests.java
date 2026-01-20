80de6ad291a1ec4bf241093c6d82a5ba77c30d40
eliel pop os <elielraposo10@gmail.com>
2026-01-20 10:59:59 -0300

    chore: initial commit with Angular 17 frontend and Spring Boot 17 backend

---
+++ backend/src/test/java/com/example/JAMachines/BackendJaMachinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JAMachines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJaMachinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
