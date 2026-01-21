ab923a0eb316316b3334dbc76d7e1bdab53a7ab9
eliel pop os <elielraposo10@gmail.com>
2026-01-20 22:10:31 -0300

    feat(frontend): upgrade Angular from v17 to v19

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 
-// export const routes: Routes = [];
-
-export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-];
+export const routes: Routes = [];
